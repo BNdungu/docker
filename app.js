830a134cb2980ea8a74719304d6f4d75fe71499d
BNdungu <bryannganga909@gmail.com>
2023-12-24 09:15:32 +0300

    .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req,res) => {
-    res.send('Hello world! my name is Nganga kgjgd')
+    res.send('Hello world! my name is Nganga Ndungu')
 })
 
 app.get('/docker',(req,res) => {
